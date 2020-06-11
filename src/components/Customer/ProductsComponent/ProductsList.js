c8617df1cba599bade99cd9f1dc046c19d3cc3c0
MonseSalasVi <cmonse.salasv@gmail.com>
2020-06-11 10:53:23 -0500

    login

--- src/components/Customer/ProductsComponent/ProductsList.js
+++ src/components/Customer/ProductsComponent/ProductsList.js
@@ -63,18 +63,6 @@ const ProductsList = ({productsList, handleDetailedProduct, goBack}) => {
 
     )
 
-    /*
-    <div className='dataReturn'>
-        <img src={data.picture} alt='t-shirtimage' style={imageStyle}/>
-        <p key={data.price}>{data.price}</p>
-        <p key={data.description}>{data.description}</p>
-    </div>
-    )*/
-       /* const imageStyle = {
-        width: 200,
-        height: 250,
-    }*/
-
 
     return(
         <div className={classes.listContainer} >
@@ -85,37 +73,3 @@ const ProductsList = ({productsList, handleDetailedProduct, goBack}) => {
 
 export default ProductsList;
 
-
-// root: {
-
-//     background:"pink",
-//     display: "flex",
-//     flexWrap: "wrap",
-//   },
-//   conainermedia:{
-//       display: "flex",
-//       flexWrap: "wrap",
-//       alingItems: "center"
-
-//   },
-//   media: {
-//       width: 200,
-//       height: 250,
-
-//   },
-//   container:{
-//       display: "flex",
-//       flexWrap: "wrap",
-// 	  flexDirection: "row",
-// 	  justifyContent: 'space-between',
-//       padding: "5px"
-
-//   },
-//   description:{
-//        width: 200,
-//       height: 50,
-//   },
-//   listContainer:{
-//       width:'100%',
-//       background:"blue"
-//   }
--- src/views/Login.js
+++ src/views/Login.js
@@ -5,6 +5,7 @@ const Login = () => {
 	return(
 		<>
 		<LoginForm/>
+		
 		</>
 	)
 
