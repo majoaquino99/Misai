22b46740d1e0bd4bfc960a90aefde3ffe2afef98
majoaquino99 <majo.aqh@gmail.com>
2020-06-10 21:35:32 -0500

    Adding states for categories buttons

--- src/components/Customer/ProductsComponent/ProductsList.js
+++ src/components/Customer/ProductsComponent/ProductsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// Products Component receives an object with Image, price, and Description depending 
+// Products Component receives an object with Image, price, and Description depending
 // on the value received by categories
 // You need to create an infinite scroll grid including 5-6 products on each row
 // Condition the image size to be the same for each one
