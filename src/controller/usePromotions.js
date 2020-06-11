6b72ca92f9fd425da3b37208bbd644bb0a7e083f
Mirey Morales <mirey.morales91@gmail.com>
2020-06-11 15:34:58 -0500

    API image Categories/Products/Promotions

--- src/controller/usePromotions.js
+++ src/controller/usePromotions.js
@@ -1,18 +1,24 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const usePromotions = () => {
+const usePromotions = (promotion) => {
     const [promotions, setPromotions] = useState([]);
     const [errorPromotion, setErrorPromotion] = useState(null)
+    const [imagePromotions, setImagePromotions] = useState('')
 
     useEffect(() => {
         const fetchDataPromotions = () => {
+
+            const host = `/v1/shop/promotions/${promotion}/image`
+            const getImagePromotionsAPI = `https://genericapiv1.azurewebsites.net${host}`
+
             const getPromotionsAPI = 'https://genericapiv1.azurewebsites.net/v1/shop/promotions'
             const getAllPromotionss = axios.get(getPromotionsAPI)
             axios.all([getAllPromotionss])
                 .then(axios.spread((...allData) => {
                     const allDataPromotionss = allData[0].data
                     setPromotions(allDataPromotionss)
+                    setImagePromotions(getImagePromotionsAPI)
                 })
                 ).catch((e) => {
                     setErrorPromotion(e)
@@ -22,6 +28,7 @@ const usePromotions = () => {
     }, [])
     return {
         promotions,
+        imagePromotions,
         errorPromotion
     }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -6,27 +6,32 @@ import useCategories from '../controller/useCategories';
 import useProducts from '../controller/useProducts';
 import useProductById from '../controller/useProductById';
 import usePromotions from '../controller/usePromotions';
+
+//import useImageCategories from '../controller/useImage'
 // import Women from './v/Women'
 // import Man from './v/Man'
 
 
 function Home() {
 
-	const {categories, errorCategories} = useCategories();
-	const {products, errorProducts} = useProducts();
+	const {categories, imageCategories, errorCategories} = useCategories(1);
+	const {products, imageProductos, errorProducts} = useProducts(3);
 	const {product, errorProduct} = useProductById(1);
-	const {promotions, errorPromotion} = usePromotions();
-	console.log(product)
+	const {promotions, imagePromotions, errorPromotion} = usePromotions(4);
+	//const {imageCategories, errorImage} = useImageCategories();
+
+	//console.log(product)
 	console.log(categories);
-	console.log(products);
-	console.log(promotions);
+	//console.log(products);
+	//console.log(promotions);
+	console.log(imagePromotions)
 
 	let categoriesSection = (<h1>Loading</h1>);
 	if (categories.length > 0) {
 		categoriesSection = categories.map((category) => {
 			return (
 				<div>
-					{category.categoryID}, {category.description}
+					{category.categoryID}, {category.description}, {imageCategories}
 				</div>
 			);
 		});
